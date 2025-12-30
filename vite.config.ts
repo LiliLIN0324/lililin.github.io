84d1ace3359fe91aa751b5cc1b82f77d8d2e6151
lili <lily1326685527@gmail.com>
2025-12-30 10:26:56 +0900

    compress img

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-
 export default defineConfig({
   base: '/',
-  
   plugins: [react()],
   resolve: {
     alias: {
@@ -17,13 +15,18 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          // 将 node_modules 中的库打包到独立的 vendor 文件中
           if (id.includes('node_modules')) {
+            // 把大库单独拆 chunk
             if (id.includes('three')) return 'threejs';
             if (id.includes('maplibre')) return 'maplibre';
+            if (id.includes('lucide-react')) return 'icons';
             return 'vendor';
+          }
+
+          // 把 src 下的特定大模块拆成单独 chunk
+          if (id.includes('/src/components/URplatform')) return 'URplatform';
+          if (id.includes('/src/components/App')) return 'App';
         }
-       }
       }
     }
   }
