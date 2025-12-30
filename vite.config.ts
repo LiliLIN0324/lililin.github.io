16733d76f4aca000dcfd318240d1030f5b9f760e
lili <lily1326685527@gmail.com>
2025-12-30 20:10:04 +0900

    clip

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  // base: '/',
-  base: '/lililin0324.github.io/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
